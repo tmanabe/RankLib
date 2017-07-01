52ca070c3a143ff09679cdd7f487fdd13f791ba1
Tomohiro Manabe <tmanabe@users.noreply.github.com>
2017-07-01 23:46:47 +0900

    implement SpanF

--- src/ciir/umass/edu/learning/SpanF.java
+++ src/ciir/umass/edu/learning/SpanF.java
@@ -8,19 +8,48 @@ import java.util.*;
 /**
  * @author tmanabe
  *
- * This class implements the SpanF model and its optimization with the method known as Coordinate Ascent.
+ * This class implements the Span model proposed by Song et al. [1] then
+ * naturally extended for multiple fields by tmanabe, and
+ * its optimization with the method known as Coordinate Ascent.
  *
- * You must sort kf + 1 features in the following order where
- * k means |keywords|, f |fields|, tf(i, j) TF of i-th keyword in j-th field,
- * H(i) entropy of i-th keyword, and lp(j) length penalty of j-th field.
- * [k,     H(1),     H(2),     H(3),     ..., H(k),
- *  lp(1), tf(1, 1), tf(2, 1), tf(3, 1), ..., tf(k, 1),
- *  lp(2), tf(1, 2), tf(2, 2), tf(3, 2), ..., tf(k, 2),
- *  lp(3), tf(1, 3), tf(2, 3), tf(3, 3), ..., tf(k, 3),
+ * [1] R. Song, M. J. Taylor, J.-R. Wen, H.-W. Hon, and Y. Yu. Viewing term
+ * proximity from a different perspective. In ECIR, pages 346–357, 2008.
+ *
+ * You must sort features in the following order where
+ * k means |keywords|, f |fields|,
+ * H(j) the entoropy of j-th keyword,
+ * lp(i) the length penalty of i-th field,
+ * n(i,j) |spans of i-th field in which j-th keyword occurs|,
+ * s(h,i) |keywords occur in the h-th span of i-th field|, and
+ * w(h,i,j) the width of h-th span of i-th field in which j-th keyword occurs.
+ *
+ * [k, f, H(1), H(2), H(3), ..., H(k), lp(1), lp(2), lp(3), ..., lp(f),
+ *
+ *  n(1,1), s(1,1), w(1,1,1), s(2,1), w(2,1,1), ..., s(n(1,1),1), w(n(1,1),1,1),
+ *  n(2,1), s(1,2), w(1,2,1), s(2,2), w(2,2,1), ..., s(n(2,1),2), w(n(2,1),2,1),
+ *  n(3,1), s(1,3), w(1,3,1), s(2,3), w(2,3,1), ..., s(n(3,1),3), w(n(3,1),3,1),
  *  ...
- *  lp(f), tf(1, f), tf(2, f), tf(3, f), ..., tf(k, f)]
- * Note that H(i) is the (i+1)-th feature,
- * lp(j) the ((k+1)j+1)-th, and tf(i, j) the ((k+1)j+i+1)-th.
+ *  n(f,1), s(1,f), w(1,f,1), s(2,f), w(2,f,1), ..., s(n(f,1),f), w(n(f,1),f,1),
+ *
+ *  n(1,2), s(1,1), w(1,1,2), s(2,1), w(2,1,2), ..., s(n(1,2),1), w(n(1,2),1,2),
+ *  n(2,2), s(1,2), w(1,2,2), s(2,2), w(2,2,2), ..., s(n(2,2),2), w(n(2,2),2,2),
+ *  n(3,2), s(1,3), w(1,3,2), s(2,3), w(2,3,2), ..., s(n(3,2),3), w(n(3,2),3,2),
+ *  ...
+ *  n(f,2), s(1,f), w(1,f,2), s(2,f), w(2,f,2), ..., s(n(f,2),f), w(n(f,2),f,2),
+ *
+ *  n(1,3), s(1,1), w(1,1,3), s(2,1), w(2,1,3), ..., s(n(1,3),1), w(n(1,3),1,3),
+ *  n(2,3), s(1,2), w(1,2,3), s(2,2), w(2,2,3), ..., s(n(2,3),2), w(n(2,3),2,3),
+ *  n(3,3), s(1,3), w(1,3,3), s(2,3), w(2,3,3), ..., s(n(3,3),3), w(n(3,3),3,3),
+ *  ...
+ *  n(f,3), s(1,f), w(1,f,3), s(2,f), w(2,f,3), ..., s(n(f,3),f), w(n(f,3),f,3),
+ *
+ *  ...
+ *
+ *  n(1,k), s(1,1), w(1,1,k), s(2,1), w(2,1,k), ..., s(n(1,k),1), w(n(1,k),1,k),
+ *  n(2,k), s(1,2), w(1,2,k), s(2,2), w(2,2,k), ..., s(n(2,k),2), w(n(2,k),2,k),
+ *  n(3,k), s(1,3), w(1,3,k), s(2,3), w(2,3,k), ..., s(n(3,k),3), w(n(3,k),3,k),
+ *  ...
+ *  n(f,k), s(1,f), w(1,f,k), s(2,f), w(2,f,k), ..., s(n(f,k),f), w(n(f,k),f,k)]
  */
 public class SpanF extends CoorAscent {
 
@@ -41,18 +70,30 @@ public class SpanF extends CoorAscent {
         if(0 < samples.size()) {
             RankList sampleRL = samples.get(0);
             DataPoint sampleDP = sampleRL.get(0);
-            int k = (int)sampleDP.getFeatureValue(1);
-            f = sampleDP.getLastFeature() / (k + 1) - 1;
+            f = (int)sampleDP.getFeatureValue(2);
         } else {
             f = 0;
         }
-		weight = new double[1 + 2 * f];
-		// [k1, b(1), b(2), b(3), ..., b(f), boost(1), boost(2), boost(3), ..., boost(f)]
-        // Note that b(j) is the j-th element and also that boost(j) is the (f+j)-th.
-        weight[0] = 1.2;
-        for(int i=1;i<=f;i++)
-            weight[i] = 0.75;
-        for(int i=f+1;i<weight.length;i++)
+		weight = new double[1 + 4 * f];
+		/**
+         * [k1,
+         *  z(1), z(2), z(3), ..., z(f),
+         *  x(1), x(2), x(3), ..., x(f),
+         *  b(1), b(2), b(3), ..., b(f),
+         *  boost(1), boost(2), boost(3), ..., boost(f)]
+         *  Note that z(i) is the i-th element,
+         *  that x(i) is the (f+i)-th,
+         *  that b(i) is the (2f+i)-th, and
+         *  that boost(i) is the (3f+i)-th.
+         */
+        weight[0] = 0.4;
+        for(int i=0*f+1;i<=1*f;i++)
+            weight[i] = 0.55;
+        for(int i=1*f+1;i<=2*f;i++)
+            weight[i] = 0.25;
+        for(int i=2*f+1;i<=3*f;i++)
+            weight[i] = 0.3;
+        for(int i=3*f+1;i<=4*f;i++)
             weight[i] = 1.0;
 		PRINTLN("[Done]");
 	}
@@ -60,10 +101,14 @@ public class SpanF extends CoorAscent {
     {
         if(weight_index <= 0) {
             return "k1";
-        } else if(weight_index <= f) {
-            return "b(" + weight_index +")";
+        } else if(weight_index <= 1*f) {
+            return "z(" + (weight_index - 0*f)+")";
+        } else if(weight_index <= 2*f) {
+            return "x(" + (weight_index - 1*f) +")";
+        } else if(weight_index <= 3*f) {
+            return "b(" + (weight_index - 2*f) +")";
         } else {
-            return "boost(" + (weight_index - f) + ")";
+            return "boost(" + (weight_index - 3*f) + ")";
         }
     }
     public void learn()
@@ -87,11 +132,16 @@ public class SpanF extends CoorAscent {
             int consecutive_fails = 0;
 
             //initialize weight vector
-            weight[0] = 1.2;
-            for(int i=1;i<=f;i++)
-                weight[i] = 0.75f;
-            for(int i=f+1;i<weight.length;i++)
-                weight[i] = 1.0f;
+            weight[0] = 0.4;
+            for(int i=0*f+1;i<=1*f;i++)
+                weight[i] = 0.55;
+            for(int i=1*f+1;i<=2*f;i++)
+                weight[i] = 0.25;
+            for(int i=2*f+1;i<=3*f;i++)
+                weight[i] = 0.3;
+            for(int i=3*f+1;i<=4*f;i++)
+                weight[i] = 1.0;
+
             double startScore = scorer.score(rank(samples));//compute all the scores (in whatever metric specified) and store them as cache
 
             //local best (within the current restart cycle)
@@ -129,7 +179,7 @@ public class SpanF extends CoorAscent {
 
                             if(current_weight <= 0 && w <= 0) {  // k1
                                 break;
-                            } else if(0 < current_weight && current_weight <= f && (w < 0 || 1.0d < w)) {  // b
+                            } else if(2*f < current_weight && current_weight <= 3*f && (w < 0 || 1.0 < w)) {  // b
                                 break;
                             }
 
@@ -232,15 +282,32 @@ public class SpanF extends CoorAscent {
 	public double eval(DataPoint p)
     {
         int k = (int)p.getFeatureValue(1);
-		double score = 0.0;
-        for(int i=1; i<=k; i++) {
-            double w = 0.0;
-            for(int j=1; j<=f; j++) {
-                double numer = p.getFeatureValue((k+1)*j+i+1) * weight[f+j];
-                double denom = 1.0 - weight[j] + weight[j] * p.getFeatureValue((k+1)*j+1);
-                if(0.0 < denom) w += numer / denom;
+        assert f == (int) p.getFeatureValue(2);
+        double[] hs = new double[k+1];
+        double[] lps = new double[f+1];
+        int idx = 3;
+        for (int j=1; j<=k; ++j) {
+            hs[j] = p.getFeatureValue(idx++);
+        }
+        for (int i=1; i<=f; ++i) {
+            lps[i] = p.getFeatureValue(idx++);
+        }
+        double score = 0.0;
+        for (int j=1; j<=k; ++j) {
+            double keyword_score = 0.0;
+            for (int i=1; i<=f; ++i) {
+                double field_score = 0.0;
+                double b = weight[2*f+i];
+                int n = (int) p.getFeatureValue(idx++);
+                for (int h=1; h<=n; ++h) {
+                    field_score +=
+                            Math.pow(p.getFeatureValue(idx++), weight[i]) /
+                                    Math.pow(p.getFeatureValue(idx++), weight[f+i]);
+                }
+                keyword_score += weight[3*f+i] /
+                        (1 - b + b * lps[i]) * field_score;
             }
-            score += w / (w + weight[0]) * p.getFeatureValue(i+1);
+            score += keyword_score / (weight[0] + keyword_score) * hs[j];
         }
  		return score;
 	}
--- src/ciir/umass/edu/learning/TestSpanF.java
+++ src/ciir/umass/edu/learning/TestSpanF.java
@@ -12,18 +12,18 @@ import static org.junit.Assert.*;
 
 public class TestSpanF {
     @Test
-    public void testGetLastFeature() {
-        DataPoint dp0 = new DataPoint("0 qid:0 1:0 #did:0");
-        assertEquals(1, dp0.getLastFeature());
-        DataPoint dp1 = new DataPoint("0 qid:0 1:0 2:0 #did:0");
-        assertEquals(2, dp1.getLastFeature());
-    }
-    @Test
     public void testGetK() {
         DataPoint dp0 = new DataPoint("0 qid:0 1:7 #did:0");
         assertEquals(7, (int)dp0.getFeatureValue(1));
         DataPoint dp1 = new DataPoint("0 qid:0 1:7.0 #did:0");
-        assertEquals(7, (int)dp0.getFeatureValue(1));
+        assertEquals(7, (int)dp1.getFeatureValue(1));
+    }
+    @Test
+    public void testGetF() {
+        DataPoint dp0 = new DataPoint("0 qid:0 1:0 2:7 #did:0");
+        assertEquals(7, (int)dp0.getFeatureValue(2));
+        DataPoint dp1 = new DataPoint("0 qid:0 1:0 2:7.0 #did:0");
+        assertEquals(7, (int)dp1.getFeatureValue(2));
     }
     private void _testInit(double[] expected_ws, int expected_f, SpanF spanf) {
         assertEquals(expected_f, spanf.f);
@@ -36,29 +36,30 @@ public class TestSpanF {
         List<RankList> samples = new ArrayList<>();
         SpanF spanf = new SpanF(samples, null);
         spanf.init();
-        double[] expect = {1.2};
+        double[] expect = {0.4};
         _testInit(expect, 0, spanf);
     }
     @Test
-    public void testInit_k1f1() {
+    public void testInit_k1f1s1() {
         List<RankList> samples = new ArrayList<>();
-        DataPoint dp0 = new DataPoint("0 qid:0 1:1 2:-1.0 3:1.0 4:7 #did=0");
+        DataPoint dp0 = new DataPoint(
+            "0 qid:0 1:1 2:1 3:1.0 4:1.0 5:1 6:1 7:10 #did=0"
+        );
         RankList rl0 = new RankList();
         rl0.add(dp0);
         samples.add(rl0);
         SpanF spanf = new SpanF(samples, null);
         spanf.init();
-        double[] expect = {1.2, 0.75, 1.0};
+        double[] expect = {0.4, 0.55, 0.25, 0.3, 1.0};
         _testInit(expect, 1, spanf);
     }
     @Test
-    public void testInit_k2f2() {
+    public void testInit_k2f2s2() {
         List<RankList> samples = new ArrayList<>();
         StringBuilder sb = new StringBuilder();
-        sb.append("0 qid:0 ");
-        sb.append("1:2 2:-1.0 3:0.0 ");
-        sb.append("4:1.0 5:1 6:2 ");
-        sb.append("7:2.0 8:3 9:4 ");
+        sb.append("0 qid:0 1:2 2:2 3:0.5 4:2.0 5:0.5 6:2.0 ");
+        sb.append("7:2 8:2 9:20 10:1 9:11 ");
+        sb.append("12:2 13:1 14:5 15:2 16:40 ");
         sb.append("#did=0");
         DataPoint dp0 = new DataPoint(sb.toString());
         RankList rl0 = new RankList();
@@ -66,66 +67,80 @@ public class TestSpanF {
         samples.add(rl0);
         SpanF spanf = new SpanF(samples, null);
         spanf.init();
-        double[] expect = {1.2, 0.75, 0.75, 1.0, 1.0};
+        double[] expect = {0.4, 0.55, 0.55, 0.25, 0.25, 0.3, 0.3, 1.0, 1.0};
         _testInit(expect, 2, spanf);
     }
     @Test
     public void testWeightNameOf() {
         List<RankList> samples = new ArrayList<>();
-        DataPoint dp0 = new DataPoint("0 qid:0 1:1 2:-1.0 3:1.0 4:7 #did=0");
+        StringBuilder sb = new StringBuilder();
+        sb.append("0 qid:0 1:2 2:2 3:0.5 4:2.0 5:0.5 6:2.0 ");
+        sb.append("7:2 8:2 9:20 10:1 9:11 ");
+        sb.append("12:2 13:1 14:5 15:2 16:40 ");
+        sb.append("#did=0");
+        DataPoint dp0 = new DataPoint(sb.toString());
         RankList rl0 = new RankList();
         rl0.add(dp0);
         samples.add(rl0);
         SpanF spanf = new SpanF(samples, null);
         spanf.init();
         assertEquals("k1", spanf.weightNameOf(0));
-        assertEquals("b(1)", spanf.weightNameOf(1));
-        assertEquals("boost(1)", spanf.weightNameOf(2));
+        assertEquals("z(1)", spanf.weightNameOf(1));
+        assertEquals("z(2)", spanf.weightNameOf(2));
+        assertEquals("x(1)", spanf.weightNameOf(3));
+        assertEquals("x(2)", spanf.weightNameOf(4));
+        assertEquals("b(1)", spanf.weightNameOf(5));
+        assertEquals("b(2)", spanf.weightNameOf(6));
+        assertEquals("boost(1)", spanf.weightNameOf(7));
+        assertEquals("boost(2)", spanf.weightNameOf(8));
     }
     @Test
     public void testLearn_k1() {
         List<RankList> samples = new ArrayList<>();
         StringBuilder sb = new StringBuilder();
         sb.append("1 qid:0 ");
-        sb.append("1:2 2:1.0 3:1.0 ");
-        sb.append("4:1.0 5:1 6:1 ");
+        sb.append("1:2 2:1 3:1.0 4:1.0 5:1.0 ");
+        sb.append("6:1 7:2 8:10 ");
+        sb.append("9:1 10:2 11:10 ");
         sb.append("#did=0");
         DataPoint dp0 = new DataPoint(sb.toString());
         sb = new StringBuilder();
         sb.append("0 qid:0 ");
-        sb.append("1:2 2:1.0 3:1.0 ");
-        sb.append("4:1.0 5:0 6:100 ");
+        sb.append("1:2 2:1 3:1.0 4:1.0 5:1.0 ");
+        sb.append("6:1 7:1 8:10 ");
+        sb.append("9:1 10:5 11:10 ");
         sb.append("#did=1");
         DataPoint dp1 = new DataPoint(sb.toString());
         RankList rl0 = new RankList();
         rl0.add(dp0);
         rl0.add(dp1);
         samples.add(rl0);
-        SpanF.verbose = false;
+        SpanF.verbose = true;
         SpanF spanf = new SpanF(samples, null) {
             @Override
             public int[] getShuffledWeights() {
-                return new int[]{0, 1, 2};
+                return new int[]{0};
             }
         };
         spanf.set(new APIAScorer());
         spanf.init();
         spanf.learn();
-        assertTrue(spanf.weight[0] < 1.2);
+        System.out.println(spanf.weight[0]);
+        assertTrue(spanf.weight[0] < 0.4);
     }
     @Test
     public void testLearn_boost() {
         List<RankList> samples = new ArrayList<>();
         StringBuilder sb = new StringBuilder();
         sb.append("1 qid:0 ");
-        sb.append("1:2 2:1.0 3:1.0 ");
-        sb.append("4:1.0 5:1 6:1 ");
+        sb.append("1:1 2:2 3:1.0 4:1.0 5:1.0 ");
+        sb.append("6:1 7:1 8:10 9:1 10:1 11:10 ");
         sb.append("#did=0");
         DataPoint dp0 = new DataPoint(sb.toString());
         sb = new StringBuilder();
         sb.append("0 qid:0 ");
-        sb.append("1:2 2:1.0 3:1.0 ");
-        sb.append("4:1.0 5:0 6:100 ");
+        sb.append("1:1 2:2 3:1.0 4:1.0 5:1.0 ");
+        sb.append("6:1 7:10 8:10 9:0 ");
         sb.append("#did=1");
         DataPoint dp1 = new DataPoint(sb.toString());
         RankList rl0 = new RankList();
@@ -136,27 +151,27 @@ public class TestSpanF {
         SpanF spanf = new SpanF(samples, null) {
             @Override
             public int[] getShuffledWeights() {
-                return new int[]{2, 0, 1};
+                return new int[]{7, 8};
             }
         };
         spanf.set(new APIAScorer());
         spanf.init();
         spanf.learn();
-        assertTrue(1.0 < spanf.weight[2]);
+        assertTrue(spanf.weight[7] < spanf.weight[8]);
     }
     @Test
     public void testLearn_b() {
         List<RankList> samples = new ArrayList<>();
         StringBuilder sb = new StringBuilder();
         sb.append("1 qid:0 ");
-        sb.append("1:2 2:1.0 3:1.0 ");
-        sb.append("4:1.0 5:1 6:1 ");
+        sb.append("1:1 2:1 3:1.0 4:10.0 ");
+        sb.append("5:1 6:2 7:10 ");
         sb.append("#did=0");
         DataPoint dp0 = new DataPoint(sb.toString());
         sb = new StringBuilder();
         sb.append("0 qid:0 ");
-        sb.append("1:2 2:1.0 3:1.0 ");
-        sb.append("4:10.0 5:0 6:100 ");
+        sb.append("1:1 2:1 3:1.0 4:1.0 ");
+        sb.append("5:1 6:1 7:10 ");
         sb.append("#did=1");
         DataPoint dp1 = new DataPoint(sb.toString());
         RankList rl0 = new RankList();
@@ -167,20 +182,21 @@ public class TestSpanF {
         SpanF spanf = new SpanF(samples, null) {
             @Override
             public int[] getShuffledWeights() {
-                return new int[]{1, 2, 0};
+                return new int[]{3};
             }
         };
         spanf.set(new APIAScorer());
         spanf.init();
         spanf.learn();
-        assertTrue(0.75 < spanf.weight[1]);
+        assertTrue(spanf.weight[3] < 0.3);
+
     }
     @Test
     public void testRank_withoutDedup() {
         List<RankList> samples = new ArrayList<>();
-        DataPoint dp0 = new DataPoint("0 qid:0 1:1 2:0.5 3:2.0 4:3 #did=0");
-        DataPoint dp1 = new DataPoint("0 qid:0 1:1 2:0.5 3:2.0 4:5 #did=1");
-        DataPoint dp2 = new DataPoint("0 qid:0 1:1 2:0.5 3:2.0 4:7 #did=2");
+        DataPoint dp0 = new DataPoint("0 qid:0 1:1 2:1 3:1.0 4:1.0 5:1 6:1 7:10 #did=0");
+        DataPoint dp1 = new DataPoint("0 qid:0 1:1 2:1 3:1.0 4:1.0 5:1 6:2 7:10 #did=1");
+        DataPoint dp2 = new DataPoint("0 qid:0 1:1 2:1 3:1.0 4:1.0 5:1 6:3 7:10 #did=2");
         RankList rl0 = new RankList();
         rl0.add(dp0);
         rl0.add(dp1);
@@ -196,9 +212,9 @@ public class TestSpanF {
     @Test
     public void testRank_withDedup() {
         List<RankList> samples = new ArrayList<>();
-        DataPoint dp0 = new DataPoint("0 qid:0 1:1 2:0.5 3:2.0 4:3 #did=0");
-        DataPoint dp1 = new DataPoint("0 qid:0 1:1 2:0.5 3:2.0 4:5 #did=0");
-        DataPoint dp2 = new DataPoint("0 qid:0 1:1 2:0.5 3:2.0 4:7 #did=0");
+        DataPoint dp0 = new DataPoint("0 qid:0 1:1 2:1 3:1.0 4:1.0 5:1 6:1 7:10 #did=0");
+        DataPoint dp1 = new DataPoint("0 qid:0 1:1 2:1 3:1.0 4:1.0 5:1 6:2 7:10 #did=0");
+        DataPoint dp2 = new DataPoint("0 qid:0 1:1 2:1 3:1.0 4:1.0 5:1 6:3 7:10 #did=0");
         RankList rl0 = new RankList();
         rl0.add(dp0);
         rl0.add(dp1);
@@ -211,26 +227,30 @@ public class TestSpanF {
         assertEquals(dp2, ranking.get(0));
     }
     @Test
-    public void testEval_k1f1() {
+    public void testEval_k1f1s1() {
         List<RankList> samples = new ArrayList<>();
-        DataPoint dp0 = new DataPoint("0 qid:0 1:1 2:0.5 3:2.0 4:7 #did=0");
+        DataPoint dp0 = new DataPoint(
+                "0 qid:0 1:1 2:1 3:0.7 4:0.3 5:1 6:1 7:10 #did=0"
+        );
         RankList rl0 = new RankList();
         rl0.add(dp0);
         samples.add(rl0);
         SpanF spanf = new SpanF(samples, null);
         spanf.init();
-        double w = 7.0 * 1.0 / (1.0 - 0.75 + 0.75 * 2.0);
-        double expect = w / (w + 1.2) * 0.5;
+        double w = 1.0 / (1 - 0.3 + 0.3 * 0.3) *
+                Math.pow(1, 0.55) / Math.pow(10, 0.25);
+        double expect = w / (w + 0.4) * 0.7;
         assertEquals(expect, spanf.eval(dp0), 1.0e-6);
     }
     @Test
-    public void testEval_k2f2() {
+    public void testEval_k2f2s2() {
         List<RankList> samples = new ArrayList<>();
         StringBuilder sb = new StringBuilder();
-        sb.append("1 qid:0 ");
-        sb.append("1:2 2:-0.5 3:0.5 ");
-        sb.append("4:2.0 5:4 6:5 ");
-        sb.append("7:3.0 8:6 9:7 ");
+        sb.append("1 qid:0 1:2 2:2 3:0.1 4:0.2 5:0.3 6:0.7 ");
+        sb.append("7:2 8:1 9:5 10:2 11:10 ");
+        sb.append("12:2 13:3 14:10 15:4 16:5 ");
+        sb.append("17:2 18:5 19:5 20:6 21:10 ");
+        sb.append("22:2 23:7 24:10 25:8 26:5 ");
         sb.append("#did=0");
         DataPoint dp0 = new DataPoint(sb.toString());
         RankList rl0 = new RankList();
@@ -239,13 +259,21 @@ public class TestSpanF {
         SpanF spanf = new SpanF(samples, null);
         spanf.init();
         double w = 0.0, expect = 0.0;
-        w += 4.0 * 1.0 / (1.0 - 0.75 + 0.75 * 2.0);
-        w += 6.0 * 1.0 / (1.0 - 0.75 + 0.75 * 3.0);
-        expect += w / (w + 1.2) * -0.5;
+        w += 1.0 / (1 - 0.3 + 0.3 * 0.3) *
+                (Math.pow(1, 0.55) / Math.pow(5, 0.25) +
+                        Math.pow(2, 0.55) / Math.pow(10, 0.25));
+        w += 1.0 / (1 - 0.3 + 0.3 * 0.7) *
+                (Math.pow(3, 0.55) / Math.pow(10, 0.25) +
+                        Math.pow(4, 0.55) / Math.pow(5, 0.25));
+        expect += w / (w + 0.4) * 0.1;
         w = 0.0;
-        w += 5.0 * 1.0 / (1.0 - 0.75 + 0.75 * 2.0);
-        w += 7.0 * 1.0 / (1.0 - 0.75 + 0.75 * 3.0);
-        expect += w / (w + 1.2) * 0.5;
+        w += 1.0 / (1 - 0.3 + 0.3 * 0.3) *
+                (Math.pow(5, 0.55) / Math.pow(5, 0.25) +
+                        Math.pow(6, 0.55) / Math.pow(10, 0.25));
+        w += 1.0 / (1 - 0.3 + 0.3 * 0.7) *
+                (Math.pow(7, 0.55) / Math.pow(10, 0.25) +
+                        Math.pow(8, 0.55) / Math.pow(5, 0.25));
+        expect += w / (w + 0.4) * 0.2;
         assertEquals(expect, spanf.eval(dp0), 1.0e-6);
     }
     @Test
@@ -256,7 +284,12 @@ public class TestSpanF {
     @Test
     public void testGetShuffledWeights() {
         List<RankList> samples = new ArrayList<>();
-        DataPoint dp0 = new DataPoint("0 qid:0 1:1 2:-1.0 3:1.0 4:7 #did=0");
+        StringBuilder sb = new StringBuilder();
+        sb.append("0 qid:0 1:2 2:2 3:0.5 4:2.0 5:0.5 6:2.0 ");
+        sb.append("7:2 8:2 9:20 10:1 9:11 ");
+        sb.append("12:2 13:1 14:5 15:2 16:40 ");
+        sb.append("#did=0");
+        DataPoint dp0 = new DataPoint(sb.toString());
         RankList rl0 = new RankList();
         rl0.add(dp0);
         samples.add(rl0);
@@ -267,8 +300,8 @@ public class TestSpanF {
             gsw.setAccessible(true);
             int[] actual = (int[])gsw.invoke(spanf);
             Arrays.sort(actual);
-            assertEquals(3, actual.length);
-            for(int i = 0; i < 3; i++)
+            assertEquals(9, actual.length);
+            for(int i = 0; i < 9; i++)
                 assertEquals(i, actual[i], 1.0e-6);
         }
         catch (Exception e) {
